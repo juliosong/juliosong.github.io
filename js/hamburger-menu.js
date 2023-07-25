cbe0376aa052ad566e81b1e8d584673f0a7c2945
Julio Song <juliosong91@gmail.com>
2023-07-25 15:28:55 +0800

    updated website

--- js/hamburger-menu.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() >= 200) {        // If page is scrolled more than 50px
-        $('#hamlabel').fadeIn(200);    // Fade in the arrow
-    } else {
-        $('#hamlabel').fadeOut(200);   // Else fade out the arrow
-    }
-});
